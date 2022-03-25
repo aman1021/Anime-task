dd1bd54d5cfb7ec67d2e99b322c9c8d0e7113b53
Aman Chandel <amaniiitu32@gmail.com>
2022-03-25 11:14:31 +0530

    Anime

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,10 +5,11 @@ const Navbar = () => {
     const dispatch= useDispatch();
     const [inputValue, setInputValue] =useState("");
   return (
+     <div className='navbar'>
      <div className='input-container'>
         <input class="input-field" value={inputValue} type="text" onChange={(e)=>setInputValue(e.target.value)}></input>
         <button onClick = {()=> dispatch(fetchAnime(1,inputValue))}>GO</button>
-
+     </div>
      </div>
    
   )
